da370a4dc95158a80f0f44ea62ea48b2fc96c0af
333Rosky <147708493+333Rosky@users.noreply.github.com>
2024-04-03 03:20:38 +0200

    Data ok need to make ratios

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import statsmodels.api as sm
+import logging
 import wrds
 
+logging.basicConfig(filename='journal.log', level=logging.INFO, 
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 def retrieve_data() :
     db = wrds.Connection()
     # Retrieve S&P 500 index membership from CRSP
     # You would replace 'your_username' and 'your_password' with your actual WRDS credentials
+    logging.info("Starting the main process...")
     mse = db.raw_sql("""
                         select comnam, ncusip, namedt, nameendt, 
                         permno, shrcd, exchcd, hsiccd, ticker
@@ -28,93 +32,36 @@ def retrieve_data() :
                         SELECT gvkey, conm
                         FROM comp.company
                         """)
+    logging.info("Starting the gvkey retrieving...")
     # Créez une liste des gvkeys uniques
     gvkey_list = compustat_data['gvkey'].unique().tolist()
     gvkeys_str = "', '".join(gvkey_list)
+    logging.info(f"Processing gvkey: {gvkeys_str}")
+    logging.info("All gvkeys are recovered")
     # Interrogez Compustat pour obtenir les cash flows opérationnels
-    net_income_data = db.raw_sql("""
-        SELECT tic, datadate, ni 
+    logging.info(f"Starting the data retrieving...")
+    retrieve_data_sql = db.raw_sql(f"""
+        SELECT gvkey, datadate, conm, ni, csho, at as total_investments, lt AS total_liabilities, oiadp AS operating_income, dp AS depreciation_amortization
         FROM comp.funda 
-        WHERE indfmt='INDL' 
-        AND datafmt='STD' 
-        AND popsrc='D' 
-        AND consol='C' 
-        AND datadate='2020-12-31'
-        """)
-    print(net_income_data)
-    cash_flows_query = f"""
-        SELECT gvkey, datadate, fqtr, oancfy
-        FROM comp.fundq
         WHERE gvkey IN ('{gvkeys_str}')
         AND datadate BETWEEN '2015-01-01' AND '2023-12-31'
         AND indfmt = 'INDL'
-        AND datafmt = 'STD'
-        AND popsrc = 'D'
-        AND consol = 'C'
-        ORDER BY gvkey, datadate
-        """
-    shares_outstanding_data = db.raw_sql("""
-        SELECT tic, datadate, csho
-        FROM comp.funda
-        WHERE indfmt='INDL' 
         AND datafmt='STD' 
         AND popsrc='D' 
         AND consol='C' 
-        AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
-    """)
-    print(shares_outstanding_data.head())
-    total_investments_data = db.raw_sql("""
-    SELECT tic, datadate, at AS total_investments
-    FROM comp.funda
-    WHERE indfmt='INDL' 
-    AND datafmt='STD' 
-    AND popsrc='D' 
-    AND consol='C' 
-    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
-    """)
-    print(total_investments_data.head())
-    total_liabilities_data = db.raw_sql("""
-    SELECT tic, datadate, lt AS total_liabilities
-    FROM comp.funda
-    WHERE indfmt='INDL' 
-    AND datafmt='STD' 
-    AND popsrc='D' 
-    AND consol='C' 
-    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
-    """)
-    print(total_liabilities_data.head())
-    operating_income_data = db.raw_sql("""
-    SELECT tic, datadate, oiadp AS operating_income
-    FROM comp.funda
-    WHERE indfmt='INDL' 
-    AND datafmt='STD' 
-    AND popsrc='D' 
-    AND consol='C' 
-    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
-    """)
-    print(operating_income_data.head())
-    depreciation_amortization_data = db.raw_sql("""
-    SELECT tic, datadate, dp AS depreciation_amortization
-    FROM comp.funda
-    WHERE indfmt='INDL' 
-    AND datafmt='STD' 
-    AND popsrc='D' 
-    AND consol='C' 
-    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
-    """)
-    print(depreciation_amortization_data.head())
-
+        ORDER BY gvkey, datadate
+        """)
+    print(retrieve_data_sql)
+    logging.info(f"Processing data : {retrieve_data_sql}")
+    logging.info("All datas are recovered")
     # Exécutez la requête pour obtenir les données de cash flow
-    dfs = [net_income_data, shares_outstanding_data, total_investments_data, total_liabilities_data, operating_income_data, depreciation_amortization_data]
+    dfs = [retrieve_data_sql]
     final_df = dfs[0]
     for df in dfs[1:]:
-        final_df = pd.merge(final_df, df, on=['tic', 'datadate'], how='outer')
-    
-    # Nettoyer le DataFrame final si nécessaire
-    # Par exemple, en supprimant les lignes avec des données manquantes
-    final_df_cleaned = final_df.dropna()
-
+        final_df = pd.merge(final_df, df, on=['gvkey','datadate','conm'], how='outer')
     # Exporter le DataFrame final en fichier Excel
-    final_df_cleaned.to_excel('financial_data.xlsx', index=False)
+    final_df.to_excel('financial_data.xlsx', index=False)
+    logging.info("Excel file created")
 
-retrieve_data() 
+retrieve_data()
+logging.info("End of programme ")
