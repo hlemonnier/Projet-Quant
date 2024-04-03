472ba8f3dbac40383f70705ee56ec76f1f117f28
333Rosky <147708493+333Rosky@users.noreply.github.com>
2024-04-03 02:09:14 +0200

    Retrieve data ok need to create an excel

--- main.py
+++ main.py
@@ -32,6 +32,16 @@ def retrieve_data() :
     gvkey_list = compustat_data['gvkey'].unique().tolist()
     gvkeys_str = "', '".join(gvkey_list)
     # Interrogez Compustat pour obtenir les cash flows opérationnels
+    net_income_data = db.raw_sql("""
+        SELECT tic, datadate, ni 
+        FROM comp.funda 
+        WHERE indfmt='INDL' 
+        AND datafmt='STD' 
+        AND popsrc='D' 
+        AND consol='C' 
+        AND datadate='2020-12-31'
+        """)
+    print(net_income_data)
     cash_flows_query = f"""
         SELECT gvkey, datadate, fqtr, oancfy
         FROM comp.fundq
@@ -43,8 +53,61 @@ def retrieve_data() :
         AND consol = 'C'
         ORDER BY gvkey, datadate
         """
+    shares_outstanding_data = db.raw_sql("""
+        SELECT tic, datadate, csho
+        FROM comp.funda
+        WHERE indfmt='INDL' 
+        AND datafmt='STD' 
+        AND popsrc='D' 
+        AND consol='C' 
+        AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
+    """)
+    print(shares_outstanding_data.head())
+    total_investments_data = db.raw_sql("""
+    SELECT tic, datadate, at AS total_investments
+    FROM comp.funda
+    WHERE indfmt='INDL' 
+    AND datafmt='STD' 
+    AND popsrc='D' 
+    AND consol='C' 
+    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
+    """)
+    print(total_investments_data.head())
+    total_liabilities_data = db.raw_sql("""
+    SELECT tic, datadate, lt AS total_liabilities
+    FROM comp.funda
+    WHERE indfmt='INDL' 
+    AND datafmt='STD' 
+    AND popsrc='D' 
+    AND consol='C' 
+    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
+    """)
+    print(total_liabilities_data.head())
+    operating_income_data = db.raw_sql("""
+    SELECT tic, datadate, oiadp AS operating_income
+    FROM comp.funda
+    WHERE indfmt='INDL' 
+    AND datafmt='STD' 
+    AND popsrc='D' 
+    AND consol='C' 
+    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
+    """)
+    print(operating_income_data.head())
+    depreciation_amortization_data = db.raw_sql("""
+    SELECT tic, datadate, dp AS depreciation_amortization
+    FROM comp.funda
+    WHERE indfmt='INDL' 
+    AND datafmt='STD' 
+    AND popsrc='D' 
+    AND consol='C' 
+    AND datadate BETWEEN '2020-01-01' AND '2020-12-31'
+    """)
+    print(depreciation_amortization_data())
+    
     # Exécutez la requête pour obtenir les données de cash flow
     cash_flows_data = db.raw_sql(cash_flows_query)
     final_data = pd.merge(cash_flows_data, compustat_data[['gvkey', 'conm']], on='gvkey', how='left')
     final_data_cleaned = final_data.dropna(subset=['oancfy'])
     return tickers_str, final_data_cleaned, gvkey_list
+
+retrieve_data() 
